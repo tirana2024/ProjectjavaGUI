4494e530385773fa6a404b2ba7df1fb651b6b2f5
unknown <koladritan81@gmail.com>
2024-07-28 11:14:32 +0200

    CreateAplicationStack

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Dictionary<string,string> dict = new Dictionary<string,string>();
+            dict.Add("Artion", "23");
+            dict.Add("ani", "23");
+            dict.Add("eriselda", "23");
+            dict.Add("kei", "20");
+
+            string keyValue = dict["ani"];
+            Console.WriteLine(keyValue);
+
+
+        }
+    }
